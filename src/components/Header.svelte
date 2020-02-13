c62b78ae9d1e5d84aaea633592e1f8ae5c2e291e
Malou <malou_allertz@hotmail.com>
2020-02-13 10:15:15 +0100

    align logout button

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,33 +1,39 @@
 <script>
-    import I18n from "i18n-js";
-    import logo from "../img/logo.svg";
-    import {link, navigate} from "svelte-routing";
-    import Button from "./Button.svelte";
-    import {userLoggedIn, userRole, authToken} from "../stores/user";
+  import I18n from "i18n-js";
+  import logo from "../img/logo.svg";
+  import { link, navigate } from "svelte-routing";
+  import Button from "./Button.svelte";
+  import { userLoggedIn, userRole, authToken } from "../stores/user";
 
-    const logoutUser = () => {
-        $userLoggedIn = "";
-        $userRole = "";
-        $authToken = "";
-        navigate("/");
-    }
+  const logoutUser = () => {
+    $userLoggedIn = "";
+    $userRole = "";
+    $authToken = "";
+    navigate("/");
+  };
 </script>
 
 <style>
-    header {
-        background-color: var(--color-background-grey-dark);
-        padding-top: 4px;
-        padding-bottom: 4px;
-    }
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    padding-top: 4px;
+    padding-bottom: 4px;
+
+    background-color: var(--color-background-grey-dark);
+  }
 </style>
 
 <header class="header">
-    <a href="/" use:link>
-        {@html logo}
-    </a>
-    {#if $userLoggedIn}
-        <span>
-            <Button label={I18n.t("header.logout")} onClick={logoutUser} className="cancel small"/>
-        </span>
-    {/if}
+  <a href="/" use:link>
+    {@html logo}
+  </a>
+  {#if $userLoggedIn}
+    <Button
+      label={I18n.t('header.logout')}
+      onClick={logoutUser}
+      className="cancel xs" />
+  {/if}
 </header>
