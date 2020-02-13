9384df81719b23016c8ca674cdbcd2aab4c3e96d
Malou <malou_allertz@hotmail.com>
2020-02-13 10:23:55 +0100

    fix growing menu items because of border

--- src/components/SideMenu.svelte
+++ src/components/SideMenu.svelte
@@ -1,71 +1,79 @@
 <script>
-    export let pages;
-    export let currentPage;
+  export let pages;
+  export let currentPage;
 
-    import { link } from 'svelte-routing';
-    import I18n from "i18n-js";
-    import chevron_left from "../icons/chevron-left.svg";
-    import chevron_right from "../icons/chevron-right.svg";
+  import { link } from "svelte-routing";
+  import I18n from "i18n-js";
+  import chevron_left from "../icons/chevron-left.svg";
+  import chevron_right from "../icons/chevron-right.svg";
 
-    let displayMenu = false;
+  let displayMenu = false;
 </script>
 
 <style>
-    .side-menu {
-        display: flex;
-        flex-direction: column;
-        background-color: var(--color-background-grey-light);
+  .side-menu {
+    display: flex;
+    flex-direction: column;
+    background-color: var(--color-background-grey-light);
+  }
+
+  button.menu-link {
+    display: none;
+    margin-top: 10px;
+  }
+
+  @media (max-width: 820px) {
+    .hide-menu-items {
+      display: none;
     }
 
     button.menu-link {
-        display: none;
-        margin-top: 10px;
+      display: inline-block;
     }
+  }
 
-    @media (max-width: 820px) {
-        .hide-menu-items {
-            display: none;
-        }
+  a.menu-item {
+    padding: 14px;
+    color: var(--color-text-grey);
+    font-weight: 600;
 
-        button.menu-link {
-            display: inline-block;
-        }
-    }
+    display: flex;
+    align-items: center;
 
-    a.menu-item {
-        padding: 14px;
-        color: var(--color-text-grey);
-        font-weight: 600;
+    --padding-right: 16px;
+    --border-width: 7px;
+    padding-right: calc(var(--padding-right) + var(--border-width));
+  }
 
-        display: flex;
-        align-items: center;
-    }
+  a.menu-item.active {
+    background-color: white;
+    border-right: var(--border-width) solid var(--color-primary-blue);
+    padding-right: var(--padding-right);
+  }
 
-    a.menu-item.active {
-        background-color: white;
-        border-right: 7px solid var(--color-primary-blue);
-    }
+  :global(.side-menu svg) {
+    fill: var(--color-text-grey);
+  }
 
-    :global(.side-menu svg) {
-        fill: var(--color-text-grey);
-    }
-
-    :global(.side-menu svg.menu-link) {
-        margin-right: 5px;
-    }
+  :global(.side-menu svg.menu-link) {
+    margin-right: 5px;
+  }
 </style>
 
-
 <div class="side-menu">
-    <button class="menu-link" on:click={() => displayMenu = !displayMenu}>
-        {@html displayMenu ? chevron_left : chevron_right}
-    </button>
-    <div class:hide-menu-items={!displayMenu}>
-        {#each pages as {path, icon}}
-            <a href={path} use:link class="menu-item" class:active={path === currentPage.path}>
-                {@html icon}
-                <span>{I18n.ts(`routes.${path}`)}</span>
-            </a>
-        {/each}
-    </div>
+  <button class="menu-link" on:click={() => (displayMenu = !displayMenu)}>
+    {@html displayMenu ? chevron_left : chevron_right}
+  </button>
+  <div class:hide-menu-items={!displayMenu}>
+    {#each pages as { path, icon }}
+      <a
+        href={path}
+        use:link
+        class="menu-item"
+        class:active={path === currentPage.path}>
+        {@html icon}
+        <span>{I18n.ts(`routes.${path}`)}</span>
+      </a>
+    {/each}
+  </div>
 </div>
