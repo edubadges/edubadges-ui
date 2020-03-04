1f0af4cde41fdfdded2044af2667bda3903416d4
carstendeijl <cdeijl@zilverline.com>
2020-03-04 16:08:11 +0100

    format

--- src/api/graphql.js
+++ src/api/graphql.js
@@ -3,26 +3,26 @@ import { authToken } from "../stores/user";
 import { config } from "../util/config";
 
 function validateResponse(res) {
-	return res.ok ?
-		res.json().then(({ data }) => data) :
-		Promise.reject(res)
+  return res.ok ?
+    res.json().then(({ data }) => data) :
+    Promise.reject(res)
 }
 
 function validFetch(query) {
-	return fetch(
-		`${config.serverUrl}/graphql`,
-		{
-			method: 'POST',
-			headers: {
-				Accept: "application/json",
-				"Content-Type": "application/json",
-				Authorization: `Bearer ${get(authToken)}`,
-			},
-			body: JSON.stringify({ query })
-		}
-	).then(validateResponse)
+  return fetch(
+    `${config.serverUrl}/graphql`,
+    {
+      method: 'POST',
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${get(authToken)}`,
+      },
+      body: JSON.stringify({ query })
+    }
+  ).then(validateResponse)
 }
 
 export const queryData = (query) => {
-	return validFetch(`query ${query}`)
-}
+  return validFetch(`query ${query}`)
+};
