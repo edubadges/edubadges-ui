4d67c59c3f960e3ff5a6c5762623511f66f7701c
Malou <malou_allertz@hotmail.com>
2020-03-05 11:48:36 +0100

    query and show issuers

--- src/routes/teachers/Issuers.svelte
+++ src/routes/teachers/Issuers.svelte
@@ -1,5 +1,45 @@
 <script>
+  import { onMount } from "svelte";
   import I18n from "i18n-js";
+  import { queryData } from "../../api/graphql";
+  import { requestProfile } from "../../api";
+
+  let scope = "";
+  let issuers = [];
+
+  const query = `{
+      issuers {
+        name,
+      }
+    }`;
+
+  onMount(() => {
+    queryData(query).then(res => {
+      issuers = res.issuers;
+    });
+
+    requestProfile()
+      .then(({ institution }) => (scope = institution.name))
+      .catch(error => console.error(error));
+  });
 </script>
 
-<h2>{I18n.t('teacher.issuers.title')}</h2>
+<style>
+  h2 span {
+    color: var(--color-text-blue);
+  }
+</style>
+
+<h2>
+  {I18n.t('teacher.issuers.title')}
+  {#if scope}
+    <span>in</span>
+    {scope}
+  {/if}
+</h2>
+
+<div class="issuers">
+  {#each issuers as issuer}
+    <div class="issuer">{issuer.name}</div>
+  {/each}
+</div>
