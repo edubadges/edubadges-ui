adbedc0161359d345605823b15d7abfc7895d690
carstendeijl <cdeijl@zilverline.com>
2020-03-09 14:58:37 +0100

    clean spacing

--- src/util/routes.js
+++ src/util/routes.js
@@ -1,14 +1,14 @@
 import { Badges, Issuers } from "../routes/teachers";
 
 export const teacherMainRoutes = [
-	{
-		path: '/',
-		bookmark: 'badges',
-		component: Badges
-	},
-	{
-		path: "/issuers",
-		bookmark: 'issuers',
-		component: Issuers
-	}
+  {
+    path: '/',
+    bookmark: 'badges',
+    component: Badges
+  },
+  {
+    path: "/issuers",
+    bookmark: 'issuers',
+    component: Issuers
+  }
 ]
