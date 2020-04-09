455ac8c9ab32815739efde8b9a0e3f441cd64547
Malou <malou_allertz@hotmail.com>
2020-04-09 10:02:08 +0200

    faculty: move errors & submit logic to form

--- src/components/teachers/FacultyEdit.svelte
+++ src/components/teachers/FacultyEdit.svelte
@@ -2,7 +2,6 @@
   import { onMount } from "svelte";
   import { FacultyForm } from "../teachers";
   import { queryData } from "../../api/graphql";
-  import { editFaculty } from "../../api";
 
   export let entityId;
 
@@ -14,21 +13,10 @@
   }`;
 
   let faculty = {};
-  let errors = {};
 
   onMount(() => {
-    queryData(query).then(res => {
-      faculty = res.faculty;
-    });
+    queryData(query).then(res => (faculty = res.faculty));
   });
-
-  function handleSubmit() {
-    errors = {};
-
-    editFaculty(entityId, faculty)
-      .then(res => console.log("succes", res))
-      .catch(err => err.then(res => (errors = res)));
-  }
 </script>
 
-<FacultyForm {faculty} {errors} {handleSubmit} />
+<FacultyForm {faculty} {entityId} />
--- src/components/teachers/FacultyNew.svelte
+++ src/components/teachers/FacultyNew.svelte
@@ -1,17 +1,5 @@
 <script>
   import { FacultyForm } from "../teachers";
-  import { createFaculty } from "../../api";
-
-  let faculty = {};
-  let errors = {};
-
-  function handleSubmit() {
-    errors = {};
-
-    createFaculty(faculty)
-      .then(res => console.log("succes", res))
-      .catch(err => err.then(res => (errors = res)));
-  }
 </script>
 
-<FacultyForm {faculty} {errors} {handleSubmit} create />
+<FacultyForm />
