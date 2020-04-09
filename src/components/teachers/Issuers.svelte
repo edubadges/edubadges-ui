5bad243b9a2efb899c354c41f9b5c609c4826db8
Malou <malou_allertz@hotmail.com>
2020-04-09 10:47:00 +0200

    format

--- src/components/teachers/Issuers.svelte
+++ src/components/teachers/Issuers.svelte
@@ -1,18 +1,28 @@
 <script>
-  import {onMount} from "svelte";
-  import {navigate} from "svelte-routing";
+  import { onMount } from "svelte";
+  import { navigate } from "svelte-routing";
   import I18n from "i18n-js";
-  import {Table} from "../teachers";
-  import {search} from "../../util/searchData";
-  import {sort, sortType} from "../../util/sortData";
+  import { Table } from "../teachers";
+  import { search } from "../../util/searchData";
+  import { sort, sortType } from "../../util/sortData";
 
   export let mayCreate;
   export let issuers = [];
   export let facultyName = "";
 
   const tableHeaders = [
-    {name: I18n.t("teacher.name"), attribute: "name", reverse: false, sortType: sortType.ALPHA},
-    {name: I18n.t("teacher.badgeclasses.title"), attribute: "badgeclasses", reverse: true, sortType: sortType.COLLECTION}
+    {
+      name: I18n.t("teacher.name"),
+      attribute: "name",
+      reverse: false,
+      sortType: sortType.ALPHA
+    },
+    {
+      name: I18n.t("teacher.badgeclasses.title"),
+      attribute: "badgeclasses",
+      reverse: true,
+      sortType: sortType.COLLECTION
+    }
   ];
 
   $: table = {
@@ -32,21 +42,16 @@
     issuerSort.reverse,
     issuerSort.sortType
   );
-
 </script>
 
-<Table
-  {...table}
-  bind:search={issuerSearch}
-  bind:sort={issuerSort}
-  {mayCreate}>
+<Table {...table} bind:search={issuerSearch} bind:sort={issuerSort} {mayCreate}>
   {#each sortedFilteredIssuers as issuer (issuer.entityId)}
     <tr
       class="click"
       on:click={() => navigate(`/manage/issuer/${issuer.entityId}`)}>
       <td>
         {issuer.name}
-        <br/>
+        <br />
         <span class="sub-text">({facultyName || issuer.faculty.name})</span>
       </td>
       <td>{issuer.badgeclasses.length}</td>
