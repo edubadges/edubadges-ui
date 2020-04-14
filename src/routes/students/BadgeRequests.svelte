9c78bc61379c136e0a3334df6fad7260c7fceaf0
smoquet <smoquet@users.noreply.github.com>
2020-04-14 17:41:58 +0200

    changes enrollments urls to new scheme

--- src/routes/students/BadgeRequests.svelte
+++ src/routes/students/BadgeRequests.svelte
@@ -15,7 +15,7 @@
   $: if (provider) getRequestedBadges();
 
   function getRequestedBadges() {
-    getUnearnedBadges(provider).then(
+    getUnearnedBadges().then(
       res => (requests = res.filter(({ date_awarded }) => !date_awarded))
     );
   }
