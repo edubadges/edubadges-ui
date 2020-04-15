27685aba5c7bf6e1481865df601cebbd8361a7c6
Malou <malou_allertz@hotmail.com>
2020-04-15 16:39:22 +0200

    use search component in tables

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -8,13 +8,14 @@
   div {
     position: relative;
     width: 100%;
+    max-width: 250px;
     height: 30px;
 
     input {
       position: absolute;
       width: 100%;
       height: 100%;
-
+      font-size: 14px;
       border: var(--card-border);
       border-radius: var(--card-border-radius);
       padding-right: 30px;
