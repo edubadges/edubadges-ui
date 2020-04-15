8819fd17ccb7cc2205224fe03bacf468e56398cc
Malou <malou_allertz@hotmail.com>
2020-04-15 16:22:36 +0200

    style sidebar search

--- src/components/teachers/Search.svelte
+++ src/components/teachers/Search.svelte
@@ -1,19 +1,39 @@
 <script>
   import I18n from "i18n-js";
+  import { searchIcon } from "../../icons";
   import { search } from "../../stores/filter";
 </script>
 
-<style>
-  p {
-    font-weight: bold;
-  }
-
-  input[type="text"] {
+<style lang="scss">
+  div {
+    position: relative;
     width: 100%;
     height: 30px;
-    border: var(--card-border);
+
+    input {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+
+      border: var(--card-border);
+      border-radius: var(--card-border-radius);
+      padding-right: 30px;
+      padding-left: 10px;
+    }
+
+    :global(svg.search) {
+      position: absolute;
+      right: 5px;
+      height: 100%;
+      width: 20px;
+      fill: var(--color-primary-purple);
+    }
   }
 </style>
 
-<p>{I18n.t('teacher.sidebar.search')}</p>
-<input type="text" bind:value={$search} />
+<h5 class="purple">{I18n.t('teacher.sidebar.search')}</h5>
+
+<div>
+  <input bind:value={$search} />
+  {@html searchIcon}
+</div>
--- src/icons/index.js
+++ src/icons/index.js
@@ -9,5 +9,6 @@ export { default as institutionIcon } from "./institution.svg";
 export { default as institutionIcon2 } from "./building.svg";
 export { default as issuerIcon } from "./issuer.svg";
 export { default as navigateBack } from "./navigation-left-2.svg";
+export { default as searchIcon } from "./search.svg";
 export { default as stepChecked } from "./step-checked.svg";
 export { default as warningIcon } from "./warning.svg";
