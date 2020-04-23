df1c00875616d89ded1745356011969f28f87e3c
Malou <malou_allertz@hotmail.com>
2020-04-23 11:50:21 +0200

    extract table to component

--- src/components/teachers/badges/Awarded.svelte
+++ src/components/teachers/badges/Awarded.svelte
@@ -1,62 +1,14 @@
 <script>
-  import { onMount } from "svelte";
   import I18n from "i18n-js";
-  import moment from "moment";
-  import { Table } from "../../teachers";
-  import { queryData } from "../../../api/graphql";
-  import { sort, sortType } from "../../../util/sortData";
-  import { Button, CheckBox } from "../../../components";
+  import { Button } from "../../../components";
+  import Table from "./Table";
 
   export let assertions = [];
+
   let selection = [];
-
-  let checkAllValue = false;
-  function onCheckAll(val) {
-    selection = val ? assertions.map(({ entityId }) => entityId) : [];
-  }
-
-  function onCheckOne(val, entityId) {
-    if (val) {
-      selection.push(entityId);
-      table.checkAllValue = selection.length === assertions.length;
-    } else {
-      selection = selection.filter(id => id !== entityId);
-      table.checkAllValue = false;
-    }
-  }
-
-  let tableHeaders = [
-    {
-      name: "name",
-      attribute: "name",
-      reverse: false,
-      sortType: sortType.ALPHA
-    },
-
-    {
-      name: "email",
-      attribute: "email",
-      reverse: false,
-      sortType: sortType.ALPHA
-    },
-
-    {
-      name: "created",
-      attribute: "created",
-      reverse: false,
-      sortType: sortType.ALPHA
-    }
-  ];
-
-  $: table = {
-    entity: "badgeclass",
-    title: `${I18n.t("teacher.badgeclasses.title")}`,
-    tableHeaders: tableHeaders,
-    onCheckAll
-  };
 </script>
 
-<Table {...table} withCheckAll bind:checkAllValue>
+<Table collection={assertions} bind:value={selection}>
   <span slot="check-buttons">
     <Button
       small
@@ -64,17 +16,4 @@
       action={() => {}}
       text={I18n.t('teacher.badgeRevoked.revoke')} />
   </span>
-
-  {#each assertions as { entityId, user, dateAwarded } (entityId)}
-    <tr>
-      <td>
-        <CheckBox
-          value={selection.includes(entityId)}
-          onChange={val => onCheckOne(val, entityId)} />
-      </td>
-      <td class="name">{user.firstName + ' ' + user.lastName}</td>
-      <td>{user.email}</td>
-      <td>{moment(dateAwarded).format('MMM D, YYYY')}</td>
-    </tr>
-  {/each}
 </Table>
--- src/components/teachers/badges/Requested.svelte
+++ src/components/teachers/badges/Requested.svelte
@@ -1,81 +1,21 @@
 <script>
   import I18n from "i18n-js";
-  import moment from "moment";
-  import { Table } from "../../teachers";
-  import { sort, sortType } from "../../../util/sortData";
-  import { Button, CheckBox } from "../../../components";
+  import { Button } from "../../../components";
   import { awardBadges } from "../../../api";
+  import Table from "./Table";
 
   export let entityId;
   export let enrollments = [];
 
   let selection = [];
-  let checkAllValue = false;
 
   function award() {
     awardBadges(entityId, selection);
   }
-
-  function onCheckAll(val) {
-    selection = val ? enrollments.map(({ entityId }) => entityId) : [];
-  }
-
-  function onCheckOne(val, entityId) {
-    if (val) {
-      selection.push(entityId);
-      table.checkAllValue = selection.length === enrollments.length;
-    } else {
-      selection = selection.filter(id => id !== entityId);
-      table.checkAllValue = false;
-    }
-  }
-
-  const tableHeaders = [
-    {
-      name: "name",
-      attribute: "name",
-      reverse: false,
-      sortType: sortType.ALPHA
-    },
-
-    {
-      name: "email",
-      attribute: "email",
-      reverse: false,
-      sortType: sortType.ALPHA
-    },
-
-    {
-      name: "created",
-      attribute: "created",
-      reverse: false,
-      sortType: sortType.ALPHA
-    }
-  ];
-
-  $: table = {
-    entity: "badgeclass",
-    title: `${I18n.t("teacher.badgeclasses.title")}`,
-    tableHeaders: tableHeaders,
-    onCheckAll
-  };
 </script>
 
-<Table {...table} withCheckAll bind:checkAllValue>
+<Table collection={enrollments} bind:value={selection}>
   <span slot="check-buttons">
     <Button small action={award} text={I18n.t('teacher.badgeRequests.award')} />
   </span>
-
-  {#each enrollments as { entityId, user, dateCreated } (entityId)}
-    <tr>
-      <td>
-        <CheckBox
-          value={selection.includes(entityId)}
-          onChange={val => onCheckOne(val, entityId)} />
-      </td>
-      <td>{user.firstName + ' ' + user.lastName}</td>
-      <td>{user.email}</td>
-      <td>{moment(dateCreated).format('MMM D, YYYY')}</td>
-    </tr>
-  {/each}
 </Table>
