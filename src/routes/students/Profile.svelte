5436c421513ef238fb1ec44636b37a01d541a32b
Okke Harsta <oharsta@zilverline.com>
2020-04-23 11:55:56 +0200

    Pimped profile

--- src/routes/students/Profile.svelte
+++ src/routes/students/Profile.svelte
@@ -22,7 +22,7 @@
     userInVerificationFlow,
     redirectPath
   } from "../../stores/user";
-    import { TextInput } from "../../components/forms";
+  import {TextInput} from "../../components/forms";
 
   let showModal = false;
   let emails = [];
@@ -32,6 +32,11 @@
   let form;
   let error = "";
 
+  //Modal
+  let modalTitle;
+  let modalQuestion;
+  let modalAction;
+
   const setData = () => {
     Promise.all([getProfile(), getSocialAccount(), getEmails()]).then(res => {
       profile = res[0];
@@ -40,26 +45,44 @@
     });
   };
 
-  const makePrimary = emailId =>
-    setPrimaryEmail(emailId)
-      .then(setData)
-      .catch(err => {
-        err.then(res => {
-          I18n.t(["error", res.fields.error_code]);
+  const makePrimary = (showConfirmation, emailId) => {
+    if (showConfirmation) {
+      modalTitle = I18n.t("profile.makePrimary");
+      modalQuestion = I18n.t("profile.makePrimaryConfirmation");
+      modalAction = () => makePrimary(false, emailId);
+      showModal = true;
+    } else {
+      showModal = false;
+      setPrimaryEmail(emailId)
+        .then(setData)
+        .catch(err => {
+          err.then(res => {
+            I18n.t(["error", res.fields.error_code]);
+          });
         });
-      });
+    }
+  }
 
-  const removeEmail = emailId =>
-    deleteEmail(emailId)
-      .then(setData)
-      .catch(err => {
-        err.then(res => {
-          error = I18n.t(["error", res.fields.error_code]);
+  const removeEmail = (showConfirmation, emailId) => {
+    if (showConfirmation) {
+      modalTitle = I18n.t("profile.deleteEmail");
+      modalQuestion = I18n.t("profile.deleteEmailConfirmation");
+      modalAction = () => removeEmail(false, emailId);
+      showModal = true;
+
+    } else {
+      showModal = false;
+      deleteEmail(emailId)
+        .then(setData)
+        .catch(err => {
+          err.then(res => {
+            error = I18n.t(["error", res.fields.error_code]);
+          });
         });
-      });
+    }
+  }
 
   const submitEmail = email => {
-    debugger;
     addEmail(email)
       .then(res => {
         setData();
@@ -75,8 +98,12 @@
 
   const deleteProfileAction = showConfirmation => () => {
     if (showConfirmation) {
-      showModal = true
+      showModal = true;
+      modalTitle = I18n.t("profile.deleteAccount");
+      modalQuestion = I18n.t("profile.deleteAccountConfirmation");
+      modalAction = deleteProfileAction(false);
     } else {
+      showModal = false;
       deleteProfile().then(() => {
         $userLoggedIn = "";
         $userRole = "";
@@ -94,68 +121,138 @@
 </script>
 
 <style>
-  ul {
-    list-style: initial;
-    margin-left: 20px;
+  h3 {
+    font-size: 22px;
+    margin-bottom: 20px;
+    border-left: 2px solid var(--purple-2);
+    padding-left: 25px;
   }
 
-  li {
-    margin-bottom: 16px;
+  table {
+    border-collapse: collapse;
+    width: 100%;
+  }
+
+  thead {
+    color: purple;
+    border-bottom: 3px solid var(--grey-3);
+    text-align: left;
+  }
+
+  tbody td:first-child {
+    width: 50%;
+  }
+
+  tbody td {
+    padding: 10px;
   }
 
   div:not(:last-child) {
-    margin-bottom: 25px;
+    margin-bottom: 45px;
+  }
+
+  span.unverified {
+    font-style: italic;
   }
 
   .error {
     color: red;
   }
+
+  p.account-info {
+    margin-bottom: 25px;
+  }
 </style>
 
 <div>
-  <h4>{I18n.t("profile.profile")}</h4>
+  <h3>{I18n.t("profile.profile")}</h3>
 
   {#if profile}
-    <p>{I18n.t("profile.name")}: {profile.first_name} {profile.last_name}</p>
-    <p>{I18n.t("profile.primary")}: {profile.email}</p>
+    <table class="profile">
+      <thead>
+      <tr>
+        <th>{I18n.t("profile.name")}</th>
+        <th>{I18n.t("profile.primary")}</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>{profile.last_name}</td>
+        <td>{profile.email}</td>
+      </tr>
+      </tbody>
+    </table>
   {/if}
 </div>
 
 <div>
-  <h4>{I18n.t("profile.emails")}</h4>
+  <h3>{I18n.t("profile.emails")}</h3>
 
-  <ul>
+  <table class="emails">
+    <thead>
+    <tr>
+      <th>{I18n.t("profile.email")}</th>
+      <th></th>
+    </tr>
+    </thead>
+    <tbody>
     {#each emails as { email, verified, id } (id)}
-      <li>
-        {I18n.t("profile.email")}: {email}
-        <br/>
-        <Button disabled={!verified} action={() => makePrimary(id)} text={I18n.t("profile.makePrimary")}/>
-        {#if !verified}(unverified){/if}
+      <tr>
+        <td>{email}
+          {#if !verified}
+            <span class="unverified"> - {I18n.t("profile.unverified")}</span>
+          {/if}
+        </td>
+        <td>
+          <div class="actions">
+            <Button disabled={!verified} action={() => makePrimary(true, id)} text={I18n.t("profile.makePrimary")}/>
 
-        <br/>
-        <Button action={() => removeEmail(id)} text={I18n.t("profile.delete")}/>
-      </li>
+            <Button action={() => removeEmail(true, id)} text={I18n.t("profile.delete")}/>
+          </div>
+        </td>
+      </tr>
     {/each}
-  </ul>
+
+    </tbody>
+  </table>
 </div>
 
 <div>
-  <h4>{I18n.t("profile.addEmail")}</h4>
-
-  <TextInput bind:value={email}/>
-  <Button action={() => submitEmail(email)} text={I18n.t("profile.submit")}/>
+  <h3>{I18n.t("profile.addEmail")}</h3>
+  <table>
+    <thead>
+    <tr>
+      <th>{I18n.t("profile.addEmailInfo")}</th>
+      <th></th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+      <td>
+        <TextInput bind:value={email}/>
+      </td>
+      <td>
+        <Button action={() => submitEmail(email)} text={I18n.t("profile.submit")}/>
+      </td>
+    </tr>
+    </tbody>
+  </table>
   {#if error}
     <p class="error">{error}</p>
   {/if}
 </div>
 
 <div>
+  <h3>{I18n.t("profile.account")}</h3>
+  <p class="account-info">{I18n.t("profile.accountInfo")}</p>
   <Button action={deleteProfileAction(true)} text={I18n.t("profile.deleteAccount")}/>
 </div>
 
 {#if showModal}
-  <Modal submit={deleteProfileAction(false)}
+  <Modal submit={modalAction}
          cancel={() => showModal = false}
-         question={I18n.t("profile.deleteAccountConfirmation")}
-           title={I18n.t("profile.deleteAccount")}></Modal>
+         question={modalQuestion}
+           title={modalTitle}>
+  </Modal>
+
 {/if}
