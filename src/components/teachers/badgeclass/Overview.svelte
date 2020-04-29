a54d0929b68818701e3c3b1d817da7da23fa69a2
carstendeijl <cdeijl@zilverline.com>
2020-04-29 10:08:16 +0200

    withdraw enrollment navigate fix

--- src/components/teachers/badgeclass/Overview.svelte
+++ src/components/teachers/badgeclass/Overview.svelte
@@ -24,7 +24,7 @@
 
   const withdrawEnrollment = (enrollId) => {
     withdrawRequestBadge(enrollId).then(() => {
-      navigate('/requested-badges');
+      navigate('/badge-requests');
     })
   };
 </script>
