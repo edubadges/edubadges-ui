b3c3c109c9c6c4b7c988ad6084deb24fa8726182
Okke Harsta <oharsta@zilverline.com>
2021-05-27 10:35:25 +0200

    Do not fail on no terms

--- src/routes/AcceptInstitutionTerms.svelte
+++ src/routes/AcceptInstitutionTerms.svelte
@@ -1,61 +1,69 @@
 <script>
-  import {onMount} from "svelte";
-  import {Button, Spinner} from "../components";
-  import I18n from "i18n-js";
-  import {ModalTerms} from "../components/forms";
-  import termsIcon from "../icons/voorwaarden-icon1.svg";
-  import {fetchMarkdown} from "../api/markdown";
+    import {onMount} from "svelte";
+    import {Button, Spinner} from "../components";
+    import I18n from "i18n-js";
+    import {ModalTerms} from "../components/forms";
+    import termsIcon from "../icons/voorwaarden-icon1.svg";
+    import {fetchMarkdown} from "../api/markdown";
 
-  export let userHasAgreed;
-  export let userDisagreed;
-  export let badgeClass;
+    export let userHasAgreed;
+    export let userDisagreed;
+    export let badgeClass;
 
-  let loaded = false;
-  let showModalTerms = false;
-  let termsUrl;
-  let termsTitle;
+    let loaded = false;
+    let showModalTerms = false;
+    let termsUrl;
+    let termsTitle;
 
-  let excerptMarkDown;
-  let statementRawUrl;
+    let excerptMarkDown;
+    let statementRawUrl;
 
-  onMount(() => {
-    const currentLanguage = I18n.locale;
-    termsUrl = badgeClass.terms.termsUrl.find(({language}) => language.toLowerCase() === currentLanguage.toLowerCase()).url;
-    fetchMarkdown(termsUrl).then(res => {
-      excerptMarkDown = res;
-      loaded = true;
+    onMount(() => {
+        const currentLanguage = I18n.locale;
+        let termsUrl = badgeClass.terms.termsUrl.find(({language}) => language.toLowerCase() === currentLanguage.toLowerCase());
+        if (!termsUrl && badgeClass.terms.termsUrl.length > 0) {
+            termsUrl = badgeClass.terms.termsUrl[0];
+        }
+        if (!termsUrl) {
+            userHasAgreed();
+            return;
+        }
+        const url = termsUrl.url;
+        fetchMarkdown(url).then(res => {
+            excerptMarkDown = res;
+            loaded = true;
+        });
     });
-  });
 
-  const showTerms = (title, url) => () => {
-    showModalTerms = true;
-    termsUrl = url;
-    termsTitle = title;
-  };
+    const showTerms = (title, url) => () => {
+        showModalTerms = true;
+        termsUrl = url;
+        termsTitle = title;
+    };
 
-  const closeTerms = () => showModalTerms = false;
+    const closeTerms = () => showModalTerms = false;
 
-  const setButtonText = badgeClass => {
-    if (badgeClass.formal) {
-      switch (badgeClass.issuer.faculty.institution.grondslagFormeel) {
-        case "UITVOERING_OVEREENKOMST":
-          return I18n.t(`acceptTerms.student.accept`);
-        case "GERECHTVAARDIGD_BELANG":
-          return I18n.t(`acceptTerms.student.read`);
-        case "WETTELIJKE_VERPLICHTING":
-          return I18n.t(`acceptTerms.student.read`);
-      }
-    } else {
-      switch (badgeClass.issuer.faculty.institution.grondslagInformeel) {
-        case "UITVOERING_OVEREENKOMST":
-          return I18n.t(`acceptTerms.student.accept`);
-        case "GERECHTVAARDIGD_BELANG":
-          return I18n.t(`acceptTerms.student.read`);
-        case "WETTELIJKE_VERPLICHTING":
-          return I18n.t(`acceptTerms.student.read`);
-      }
+    const setButtonText = badgeClass => {
+        if (badgeClass.formal) {
+            switch (badgeClass.issuer.faculty.institution.grondslagFormeel) {
+                case "UITVOERING_OVEREENKOMST":
+                    return I18n.t(`acceptTerms.student.accept`);
+                case "GERECHTVAARDIGD_BELANG":
+                    return I18n.t(`acceptTerms.student.read`);
+                case "WETTELIJKE_VERPLICHTING":
+                    return I18n.t(`acceptTerms.student.read`);
+            }
+        } else {
+            switch (badgeClass.issuer.faculty.institution.grondslagInformeel) {
+                case "UITVOERING_OVEREENKOMST":
+                    return I18n.t(`acceptTerms.student.accept`);
+                case "GERECHTVAARDIGD_BELANG":
+                    return I18n.t(`acceptTerms.student.read`);
+                case "WETTELIJKE_VERPLICHTING":
+                    return I18n.t(`acceptTerms.student.read`);
+            }
+        }
     }
-  }
 </script>
 
 <style lang="scss">
@@ -68,9 +76,11 @@
     padding: 40px;
     flex: 1;
   }
+
   div.header {
     display: flex;
     align-items: center;
+
     img {
       margin-left: auto;
     }
@@ -110,6 +120,7 @@
     margin-top: 20px;
     display: flex;
     width: 100%;
+
     div.cancel {
       margin-right: 25px;
     }
@@ -118,45 +129,45 @@
 
 <div class="page-container">
   <p class="content">
-  {#if loaded}
-    <div class="header">
-    <h3>{badgeClass.informal ? I18n.t("acceptTerms.badgeClassEnrollmentTerms.inFormalBadges") :
-      I18n.t("acceptTerms.badgeClassEnrollmentTerms.formalBadges")}</h3>
-      <img width="100px" src={badgeClass.issuer.faculty.institution.image} alt="Logo">
-    </div>
-    <div class="markdown-body">
-      {@html excerptMarkDown}
-    </div>
-    <div class="agree">
-      {@html termsIcon}
-      <p>
-        <span>{I18n.t(`acceptTerms.badgeClassEnrollmentTerms.statementLinkPre`)}</span>
-        <a href="/terms"
-           oncontextmenu="return false;"
-           on:click|preventDefault|stopPropagation={showTerms(
+    {#if loaded}
+      <div class="header">
+        <h3>{badgeClass.informal ? I18n.t("acceptTerms.badgeClassEnrollmentTerms.inFormalBadges") :
+            I18n.t("acceptTerms.badgeClassEnrollmentTerms.formalBadges")}</h3>
+        <img width="100px" src={badgeClass.issuer.faculty.institution.image} alt="Logo">
+      </div>
+      <div class="markdown-body">
+        {@html excerptMarkDown}
+      </div>
+      <div class="agree">
+        {@html termsIcon}
+        <p>
+          <span>{I18n.t(`acceptTerms.badgeClassEnrollmentTerms.statementLinkPre`)}</span>
+          <a href="/terms"
+             oncontextmenu="return false;"
+             on:click|preventDefault|stopPropagation={showTerms(
                 I18n.t(`acceptTerms.badgeClassEnrollmentTerms.statementTitle`),
                 termsUrl)}>
-          {I18n.t(`acceptTerms.badgeClassEnrollmentTerms.statementLink`)}
-        </a>
-        <span>{I18n.t(`acceptTerms.badgeClassEnrollmentTerms.statementLinkPost`)}</span>
-      </p>
-    </div>
-    <div class="actions">
-      <div class="cancel">
-        <Button secondary={true} text={I18n.t(`modal.cancel`)} action={userDisagreed} />
+            {I18n.t(`acceptTerms.badgeClassEnrollmentTerms.statementLink`)}
+          </a>
+          <span>{I18n.t(`acceptTerms.badgeClassEnrollmentTerms.statementLinkPost`)}</span>
+        </p>
       </div>
-      <Button text={setButtonText(badgeClass)} action={userHasAgreed} />
-    </div>
-  {:else}
-    <Spinner/>
-  {/if}
+      <div class="actions">
+        <div class="cancel">
+          <Button secondary={true} text={I18n.t(`modal.cancel`)} action={userDisagreed}/>
+        </div>
+        <Button text={setButtonText(badgeClass)} action={userHasAgreed}/>
+      </div>
+    {:else}
+      <Spinner/>
+    {/if}
 </div>
 
 
 {#if showModalTerms}
   <ModalTerms
-      title={termsTitle}
-      submit={closeTerms}
-      cancel={closeTerms}
-      url={termsUrl}/>
+    title={termsTitle}
+    submit={closeTerms}
+    cancel={closeTerms}
+    url={termsUrl}/>
 {/if}
