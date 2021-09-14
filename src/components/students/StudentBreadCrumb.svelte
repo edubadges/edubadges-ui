2f2ecc2f2358334c4714d44ebbbd2309c04bd8b6
Okke Harsta <oharsta@zilverline.com>
2021-09-14 17:50:51 +0200

    Forgot add.

---
+++ src/components/students/StudentBreadCrumb.svelte
@@ -0,0 +1,31 @@
+<script>
+
+</script>
+<style lang="scss">
+  :global(div.student-bread-crumb) {
+    padding: var(--ver-padding-m) var(--hor-padding-m);
+    min-height: 47px;
+    display: flex;
+    align-items: center;
+  }
+
+  :global(div.student-bread-crumb span.icon) {
+    height: 14px;
+    width: 14px;
+    margin: auto 4px;
+  }
+
+  :global(div.student-bread-crumb a) {
+    color: var(--text-grey-dark);
+    text-decoration: underline;
+  }
+
+  :global(div.student-bread-crumb span.current) {
+    font-weight: bold;
+  }
+
+
+</style>
+<div class="student-bread-crumb">
+  <slot/>
+</div>
