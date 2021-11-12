7cc4239c55d1007d54a2ba48ad95348b5e95b08b
Okke Harsta <oharsta@zilverline.com>
2021-11-12 10:45:50 +0100

    Hide SURF welcome awarded badges

--- src/util/config.js
+++ src/util/config.js
@@ -6,5 +6,6 @@ export const config = {
   extensionsRootUrl: "http://127.0.0.1:8000/static",
   eduId: "https://mijn.test.eduid.nl",
   isDemoEnvironment: 0,
-  importBadgeAllowed: !0
+  importBadgeAllowed: !0,
+  welcomeBadgeClassName: "Edubadge account complete"
 };
