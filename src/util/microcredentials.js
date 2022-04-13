d8b7b3f9c69df39a3fb954f4cdcef01c02dc5d8e
Frans Ward <frans.ward@surfnet.nl>
2022-04-13 16:13:33 +0200

    Added even more lines to the framework template

--- src/util/microcredentials.js
+++ src/util/microcredentials.js
@@ -4,26 +4,62 @@ export const microCredentialsFramework = {
     "url": "https://www.versnellingsplan.nl/Kennisbank/pilot-microcredentials/",
     "code": "Kwaliteitskader",
     "description": `
-##### Kwaliteitskader Pilot Microcredentials voor Professionals
+#### Deze microcredential is uitgegeven in het kader van de landelijke pilot microcredentials binnen het hoger onderwijs in Nederland
 >
-In het Nederlandse hoger onderwijs is in de afgelopen jaren een hoogwaardig stelsel van kwaliteitsborging gegroeid. Instellingen geven hier allen op hun eigen wijze kleur aan. Deze eigenheid en diversiteit kunnen beschouwd worden als een kracht van het stelsel, maar kan ook leiden tot discussie onderling wanneer gezocht wordt naar eenduidigheid. Daarom is gekozen voor een kwaliteitskader waarbinnen instellingen zoveel mogelijk (inhoudelijke) vrijheid genieten. De pilot geeft ruimte aan de instellingen om te starten met dit kader als vertrekpunt en deze al doende in gezamenlijkheid bij te schaven en verder in te vullen.
+De microcredential is een betrouwbaar certificaat waarmee professionals kunnen aantonen wat ze weten, kunnen en begrijpen na succesvolle afronding van een onderwijseenheid. De microcredential geeft met andere woorden een zelfstandige waarde aan een kleinere onderwijseenheid.
 >
-**Wat vragen we minimaal aan de (interne) kwaliteitszorg?**
+Deze microcredential is uitgegeven in het kader van de pilot microcredentials binnen het Nederlandse Hoger Onderwijs [1] en staat voor kwaliteit. Door de besturen van de koepels Vereniging Hogescholen (VH) en Universiteiten Nederland (UNL) zijn de onderstaande kwaliteitsafspraken vastgesteld, waar deze microcredential aan voldoet. 
 >
-1. De kwaliteit van het met microcredentials gecertificeerde onderwijs wordt geborgd op basis van de Standaarden en richtlijnen voor kwaliteitszorg in de Europese hogeronderwijsruimte (ESG)
-2. Het CvB is bekend met en kiest bewust voor het aanbod van de microcredentials vanuit de LLO-visie van de instelling
-3. De instelling heeft een intern kwaliteitszorg-proces ingericht voor het ontwerp, erkenning en de borging van kwaliteit van de microcredentials
-4. Er is door de instelling een orgaan of meerdere organen aangewezen welke het eindniveau van microcredential-gecertificeerd onderwijs kan garanderen
-5. Er is een vorm van onderwijsevaluatie, inspraak en mogelijkheid tot indienen klachten georganiseerd voor deelnemers van mc-gecertificeerd onderwijs
+#### Kwaliteitskader van de pilot microcredentials
+>
+**Rond de minimale (interne) kwaliteitszorg van microcredentials**
+>
+1. De kwaliteit van het met microcredentials gecertificeerde onderwijs wordt geborgd op basis van de Standaarden en richtlijnen voor kwaliteitszorg in de Europese hogeronderwijsruimte (ESG) [2].
+2. Het CvB is bekend met en kiest bewust voor het aanbod van de microcredentials vanuit de LLO-visie van de instelling.
+3. De instelling heeft een intern kwaliteitszorg-proces ingericht voor het ontwerp, erkenning en de borging van kwaliteit van de microcredentials.
+4. Er is door de instelling een orgaan of meerdere organen aangewezen welke het eindniveau van microcredential-gecertificeerd onderwijs kan garanderen.
+5. Er is een vorm van onderwijsevaluatie, inspraak en mogelijkheid tot indienen klachten georganiseerd voor deelnemers van mc-gecertificeerd onderwijs.
  
-Welke voorwaarden zijn er voor deelnemende instellingen aan het onderwijs?
- 
-1. De richtlijn voor microcredentials is dat het gaat om onderwijseenheden die niet kleiner zijn dan 3 EC en niet groter dan 30 EC
-2. Het onderwijs dat gecertificeerd wordt met een microcredential is inhoudelijk gerelateerd aan het onderwijs- en/of onderzoeksportfolio van de instelling. Dit kan zowel bestaand als nieuw (door)ontwikkeld onderwijs zijn
-3. Duidelijk is wie de beoogde doelgroep van het onderwijs is, indien nodig wat de benodigde voorkennis van de deelnemers is, wat de eventuele toelatingsvoorwaarden zijn en hoe deze getoetst worden
-4. Het onderwijsprogramma, de onderwijsleeromgeving en de kwaliteit van het docententeam maken het voor de instromende deelnemers mogelijk de beoogde leeruitkomsten te realiseren
-5. Er wordt inzichtelijk gemaakt wat de leeruitkomsten zijn en wat het onderwijsniveau en omvang is van de microcredential. De deelnemende instellingen beschrijven dit op een eenduidige manier, in lijn met Europese afspraken (Bologna) en ontwikkelingen in Brussel
-6. Instellingen erkennen in principe de (gevalideerde) leeruitkomsten van reeds en/of elders behaalde microcredentials (zie punt 5 in Q&A). Of dit eventueel leidt tot instroom en/of vrijstelling blijft vallen onder het mandaat van de Examencommissie of een ander daartoe door de instelling aangewezen orgaan
+**Voorwaarden voor de deelnemende instellingen aan het onderwijs**
+>
+1. De richtlijn voor microcredentials is dat het gaat om onderwijseenheden die niet kleiner zijn dan 3 EC en niet groter dan 30 EC.
+2. Het onderwijs dat gecertificeerd wordt met een microcredential is inhoudelijk gerelateerd aan het onderwijs- en/of onderzoeksportfolio van de instelling. Dit kan zowel bestaand als nieuw (door)ontwikkeld onderwijs zijn.
+3. Duidelijk is wie de beoogde doelgroep van het onderwijs is, indien nodig wat de benodigde voorkennis van de deelnemers is, wat de eventuele toelatingsvoorwaarden zijn en hoe deze getoetst worden.
+4. Het onderwijsprogramma, de onderwijsleeromgeving en de kwaliteit van het docententeam maken het voor de instromende deelnemers mogelijk de beoogde leeruitkomsten te realiseren.
+5. Er wordt inzichtelijk gemaakt wat de leeruitkomsten zijn en wat het onderwijsniveau en omvang is van de microcredential. De deelnemende instellingen beschrijven dit op een eenduidige manier, in lijn met Europese afspraken (Bologna) en ontwikkelingen in Brussel. **Onderstaand vindt u de aanvulling van het kwaliteitskader op dit punt**.
+6. Instellingen erkennen in principe de (gevalideerde) leeruitkomsten van reeds en/of elders behaalde microcredentials (zie punt 5 in Q&A). Of dit eventueel leidt tot instroom en/of vrijstelling blijft vallen onder het mandaat van de Examencommissie of een ander daartoe door de instelling aangewezen orgaan.
 7. De toetsen ondersteunen het leerproces van de deelnemer en de beoordeling is valide, betrouwbaar, voor deelnemers inzichtelijk en voldoende onafhankelijk.
+
+**De vastgestelde aanvulling op het kwaliteitskader rond het beschrijven van de inhoud van microcredentials**v
+>
+Voor het beschrijven van de inhoud van microcredentials nemen we het Europese kader over dat uitgaat van leerwegonafhankelijk geformuleerde learning outcomes. Learning outcomes kunnen in het Nederlands vertaald worden als leeruitkomsten en/of leerresultaten. De methodiek voor het formuleren van leeruitkomsten is aan de instelling.
+>
+In het kader van de Europese Bologna afspraken worden learning outcomes als volgt gedefinieerd [3]:
+>
+*A learning outcome is a measurable result of a learning experience which allows us to ascertain to which extent / level / standard a competence had been formed of enhanced. Learning outcomes are not properties unique to each student, but statements which allow higher education institutions to measure whether students have developed their competences to the required level.*
+>
+*Learning outcomes describe what a learner is expected to know, understand and be able to demonstrate after successful completion of a process of learning. They are statements of concrete and verifiable signs that witness/certify how the planned competences, including the required levels of knowledge, are being developed or acquired.*
+>
+**Kwaliteitskader met betrekking tot het beschrijven van het niveau van microcredentials**
+
+Het niveau van microcredentials wordt op 2 manieren beschreven:
+
+- Door middel van goed geformuleerde leeruitkomsten die de lezer een beeld van het niveau van de microcredential geven.
+- Hiernaast hanteren instellingen de indicatoren van EQF / NLQF om het niveau van de microcredential te duiden. De instelling dient dit zelf te kunnen verantwoorden.
+
+**Kwaliteitskader met betrekking tot het beschrijven van de omvang van microcredentials**
+>
+Het beschrijven van de studielast aan de hand van credits of studiebelastingsuren is breed toepasbaar voor zowel geaccrediteerd als accreditatiewaardig[4] aanbod. Dit bevordert de uitwisselbaarheid en verdient de voorkeur voor microcredentials.
+Het verbinden van credits aan (de europese normen voor) studiebelasting is voor deelnemers en werkgevers relevant zodat zij een inschatting kunnen maken van de mogelijke tijdsinvestering. Deze inschaling kan worden gebaseerd op de gemiddelde deelnemer zonder extra voorkennis.
+>
+**Voetnoten:**
+>
+[1] De pilot is gestart in oktober 2021 en kent een looptijd van twee jaar, tot en met 31 december 2023.
+>
+[2] Standard and Guidelines for Quality Asdsurance in the European Higher Education Area (ESG). (2015). Brussels, Belgium.
+>
+[3] Competences in Education an Recognition Project (CoRe). (2010). [A Tuning Guide to Formulating Degree Programme Profiles including Programma Competences and Programme Learning Outcomes.](http://tuningacademy.org/wp-content/uploads/2014/02/A-Guide-to-Formulating-DPP_EN.pdf)
+>
+[4] Accreditatiewaardig aanbod gebruiken we als werkterm om aan te geven dat het aanbod vanuit het kwaliteitskader van de pilot aan de Europese ESG standaarden voor kwaliteitszorg voldoet maar nog buiten bestaande externe kwaliteitszorginstrumenten valt. 
 `
 }
