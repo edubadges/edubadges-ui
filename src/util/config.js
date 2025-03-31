9e3bb832df5e063428c953d7bd92a968d1567942
Daniel Ostkamp <4895210+Iso5786@users.noreply.github.com>
2025-03-31 14:07:07 +0200

    Use env vars instead of custom config

--- src/util/config.js
+++ src/util/config.js
@@ -1,16 +1,18 @@
+import env from "env-var";
+
 export const config = {
-    local: true,
-    serverUrl: "http://127.0.0.1:8000",
-    studentDomain: "edu_id",
-    teacherDomain: "surf_conext",
-    extensionsRootUrl: "http://127.0.0.1:8000/static",
-    eduId: "https://mijn.test.eduid.nl",
-    isDemoEnvironment: 0,
-    isProdEnvironment: 0,
-    importBadgeAllowed: !0,
-    welcomeBadgeClassName: "Edubadge account complete",
+    local: env.get("LOCAL").default("true").asBool(),
+    serverUrl: env.get("SERVER_URL").default("http://localhost:8000").asUrlString(),
+    studentDomain: env.get("STUDENT_DOMAIN").default("edu_id").asString(),
+    teacherDomain: env.get("TEACHER_DOMAIN").default("surf_conext").asString(),
+    extensionsRootUrl: env.get("EXTENSIONS_ROOT_URL").default("http://localhost:8000/static").asUrlString(),
+    eduId: env.get("EDU_ID").default("https://mijn.test.eduid.nl").asUrlString(),
+    isProdEnvironment: env.get("IS_PROD_ENVIRONMENT").default("false").asBool(),
+    isDemoEnvironment: env.get("IS_DEMO_ENVIRONMENT").default("true").asBool(),
+    importBadgeAllowed: env.get("IMPORT_BADGE_ALLOWED").default("true").asBool(),
+    welcomeBadgeClassName: env.get("WELCOME_BADGE_CLASS_NAME").default("Edubadge account complete").asString(),
     features: {
-        endorsements: true,
+        endorsements: env.get("FEATURES_ENDORSEMENTS").default("true").asBool(),
     },
-    hostname: "localhost",
+    hostname: env.get("HOSTNAME").default("localhost").asString(),
 };
