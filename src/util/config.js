7f7a76ec89cf3d0c299588c1495f97abfef145a6
Daniel <daniel.ostkamp@surf.nl>
2025-04-04 08:09:48 +0200

    Updating env vars in config

--- src/util/config.js
+++ src/util/config.js
@@ -1,14 +1,14 @@
 export const config = {
-    serverUrl: window.SERVER_URL || "https://api.edubadges.dev.sdp.surf.nl",
-    studentDomain: window.STUDENT_DOMAIN || "edu_id",
-    teacherDomain: window.TEACHER_DOMAIN || "surf_conext",
-    extensionsRootUrl: window.EXTENSIONS_ROOT_URL || "https://api.edubadges.dev.sdp.surf.nl",
-    eduId: window.EDU_ID || "https://mijn.test.eduid.nl/personal",
-    isProdEnvironment: window.IS_PROD_ENVIRONMENT || false,
-    isDemoEnvironment: window.IS_DEMO_ENVIRONMENT || true,
-    importBadgeAllowed: window.IMPORT_BADGE_ALLOWED || true,
-    welcomeBadgeClassName: window.WELCOME_BADGE_CLASS_NAME || "Edubadge account complete",
+    serverUrl: globalThis.SERVER_URL || "https://api.edubadges.dev.sdp.surf.nl",
+    studentDomain: globalThis.STUDENT_DOMAIN || "edu_id",
+    teacherDomain: globalThis.TEACHER_DOMAIN || "surf_conext",
+    extensionsRootUrl: globalThis.EXTENSIONS_ROOT_URL || "https://api.edubadges.dev.sdp.surf.nl",
+    eduId: globalThis.EDU_ID || "https://mijn.test.eduid.nl/personal",
+    isProdEnvironment: globalThis.IS_PROD_ENVIRONMENT || false,
+    isDemoEnvironment: globalThis.IS_DEMO_ENVIRONMENT || true,
+    importBadgeAllowed: globalThis.IMPORT_BADGE_ALLOWED || true,
+    welcomeBadgeClassName: globalThis.WELCOME_BADGE_CLASS_NAME || "Edubadge account complete",
     features: {
-        endorsements: window.FEATURES_ENDORSEMENTS || true,
+        endorsements: globalThis.FEATURES_ENDORSEMENTS || true,
     }
 };
