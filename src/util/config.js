51ad2d070df5b731a9e3e300245254704cf5b921
Daniel <daniel.ostkamp@surf.nl>
2025-04-04 08:09:35 +0200

    Updating env vars in config

--- src/util/config.js
+++ src/util/config.js
@@ -1,16 +1,14 @@
 export const config = {
-    local: window.LOCAL || "true",
-    serverUrl: window.SERVER_URL || "http://localhost",
+    serverUrl: window.SERVER_URL || "https://api.edubadges.dev.sdp.surf.nl",
     studentDomain: window.STUDENT_DOMAIN || "edu_id",
     teacherDomain: window.TEACHER_DOMAIN || "surf_conext",
-    extensionsRootUrl: window.EXTENSIONS_ROOT_URL || "http://localhost:8000/static",
-    eduId: window.EDU_ID || "https://mijn.test.eduid.nl",
+    extensionsRootUrl: window.EXTENSIONS_ROOT_URL || "https://api.edubadges.dev.sdp.surf.nl",
+    eduId: window.EDU_ID || "https://mijn.test.eduid.nl/personal",
     isProdEnvironment: window.IS_PROD_ENVIRONMENT || false,
     isDemoEnvironment: window.IS_DEMO_ENVIRONMENT || true,
     importBadgeAllowed: window.IMPORT_BADGE_ALLOWED || true,
     welcomeBadgeClassName: window.WELCOME_BADGE_CLASS_NAME || "Edubadge account complete",
     features: {
         endorsements: window.FEATURES_ENDORSEMENTS || true,
-    },
-    hostname: window.HOSTNAME || "http://localhost",
+    }
 };
