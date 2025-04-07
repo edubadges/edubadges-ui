2b04bb27dcfb0527db60d6b0d9048f5ccb01b5d1
Daniel Ostkamp <4895210+Iso5786@users.noreply.github.com>
2025-04-07 07:08:44 +0200

    Fixed config

--- src/util/config.js
+++ src/util/config.js
@@ -1,8 +1,8 @@
 export const config = {
-    serverUrl: globalThis.SERVER_URL || "https://api.edubadges.dev.sdp.surf.nl",
+    serverUrl: globalThis.SERVER_URL || "https://api-edubadges.dev.sdp.surf.nl",
     studentDomain: globalThis.STUDENT_DOMAIN || "edu_id",
     teacherDomain: globalThis.TEACHER_DOMAIN || "surf_conext",
-    extensionsRootUrl: globalThis.EXTENSIONS_ROOT_URL || "https://api.edubadges.dev.sdp.surf.nl",
+    extensionsRootUrl: globalThis.EXTENSIONS_ROOT_URL || "https://api-edubadges.dev.sdp.surf.nl",
     eduId: globalThis.EDU_ID || "https://mijn.test.eduid.nl/personal",
     isProdEnvironment: globalThis.IS_PROD_ENVIRONMENT || false,
     isDemoEnvironment: globalThis.IS_DEMO_ENVIRONMENT || true,
@@ -10,5 +10,5 @@ export const config = {
     welcomeBadgeClassName: globalThis.WELCOME_BADGE_CLASS_NAME || "Edubadge account complete",
     features: {
         endorsements: globalThis.FEATURES_ENDORSEMENTS || true,
-    }
+    },
 };
