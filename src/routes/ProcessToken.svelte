b322305a01fc598faa0b2b00da758fba2cd5e106
Bèr Kessels <ber@berk.es>
2025-12-22 15:57:23 +0100

    chore: Avoid re-using the "role" enum that we've imported

--- src/routes/ProcessToken.svelte
+++ src/routes/ProcessToken.svelte
@@ -27,9 +27,9 @@
     onMount(() => {
         const urlSearchParams = new URLSearchParams(window.location.search);
         authError = urlSearchParams.get("authError");
-        const role = urlSearchParams.get("role");
-        if (role) {
-            $userRole = role;
+        const roleParam = urlSearchParams.get("role");
+        if (roleParam) {
+            $userRole = roleParam;
         }
 
         if (!authError) {
