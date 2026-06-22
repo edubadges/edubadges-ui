3d11aa110afb1e7580d6701a4d927ef2dc729f12
Arne de Laat <arne.delaat@surf.nl>
2026-06-22 10:41:35 +0200

    Add translation logic to landing.js

--- src/landing.js
+++ src/landing.js
@@ -1,5 +1,27 @@
 import Landing from "./routes/Landing.svelte";
 
+import Cookies from "js-cookie";
+import I18n from "i18n-js";
+
+import "./locale/en";
+import "./locale/nl";
+
+if (typeof window !== "undefined") {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    let lang;
+    if (urlSearchParams.has("lang")) {
+        lang = urlSearchParams.get("lang");
+    } else if (Cookies.get("lang")) {
+        lang = Cookies.get("lang");
+    } else {
+        lang = navigator.language.toLowerCase().substring(0, 2);
+    }
+    lang = ["en", "nl"].indexOf(lang) > -1 ? lang : "en";
+    I18n.locale = lang;
+} else {
+    I18n.locale = "en";
+}
+
 const app = new Landing({ target: document.body });
 
 export default app;
