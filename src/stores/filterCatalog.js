730a240aefebc26821755c3a52ac53a17dbe7909
Arne de Laat <arne.delaat@surf.nl>
2026-08-27 15:44:45 +0200

    Improve handling of archived badges
    Unless explicitly filtered on archived badges they do not count
    towards occurrences for other filters. They are only counted for the
    'Type' filter.

--- src/stores/filterCatalog.js
+++ src/stores/filterCatalog.js
@@ -32,7 +32,7 @@ export const tree = derived(
         badgeClasses, search, page, educationalLevelSelected, institutionSelected, facultySelected, issuerSelected,
         virtualOrganisationSelected, typeBadgeClassSelected, sortTarget,
     ]) => {
-        const filteredBadgeClasses = filterBySearch(badgeClasses, search)
+        const filteredBadgeClassesInitial = filterBySearch(badgeClasses, search)
             .filter(badge => {
                 return (!educationalLevelSelected.length || educationalLevelSelected.includes(badge.institutionType))
                     && (!institutionSelected.length || institutionSelected.includes(badge.issuer.faculty.institution.entityId))
@@ -41,6 +41,13 @@ export const tree = derived(
                     && (!virtualOrganisationSelected.length || virtualOrganisationSelected.includes(badge.issuer.faculty.name))
                     && (!typeBadgeClassSelected.length || typeBadgeClassSelected.find(typeBadge => badge.types.includes(typeBadge)))
             });
+
+        // Archived badges are hidden unless explicitly filtered for
+        // Also exclude these hidden badges from counts in other categories
+        let filteredBadgeClasses = filteredBadgeClassesInitial.slice();
+        if (!typeBadgeClassSelected.includes(badgeClassFilterTypes.ARCHIVED)) {
+            filteredBadgeClasses = filteredBadgeClassesInitial.filter(badge => !badge.archived);
+        };
 
         const educationLevels = filteredBadgeClasses.reduce((acc, badge) => {
                 const item = acc.find(v => v.value === badge.institutionType);
@@ -113,7 +120,8 @@ export const tree = derived(
             return acc;
         }, []);
 
-        const badgeClassTypes = filteredBadgeClasses.reduce((acc, badge) => {
+        const badgeClassTypes = filteredBadgeClassesInitial.reduce(
+            (acc, badge) => {
                 if (badge.archived) {
                     const item = acc.find(v => v.value === badgeClassFilterTypes.ARCHIVED);
                     if (item) {
@@ -155,17 +163,9 @@ export const tree = derived(
 
         const minimalPage = Math.min(page, Math.ceil(sortedBadgeClasses.length / catalogPageCount))
 
-        //Default we do not show the archived
-        const sortedBadgeClassesFiltered = sortedBadgeClasses.filter(badge => {
-            if (!typeBadgeClassSelected.includes(badgeClassFilterTypes.ARCHIVED)) {
-                return !badge.archived;
-            }
-            return true
-        });
-
         return {
-            badgeClasses: sortedBadgeClassesFiltered,
-            paginatedBadges: sortedBadgeClassesFiltered.slice((minimalPage - 1) * catalogPageCount, minimalPage * catalogPageCount),
+            badgeClasses: sortedBadgeClasses,
+            paginatedBadges: sortedBadgeClasses.slice((minimalPage - 1) * catalogPageCount, minimalPage * catalogPageCount),
             page: minimalPage,
             educationLevels: sort(educationLevels, true),
             institutions: sort(institutions, true),
